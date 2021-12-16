5feb9159b9fde26cddeaf63fea1990ec2155eb1e
HarrisonL <49885985+TaxAvoider@users.noreply.github.com>
2021-12-16 23:13:19 +1100

    WE GOT THE DATA WORKING!!

--- webscrape_test.py
+++ webscrape_test.py
@@ -1,43 +1,35 @@
 """
 Created on 24/11/2021
 Example made using https://oxylabs.io/blog/python-web-scraping
+Helpful reference for html syntax: https://www.w3schools.com/tags/default.asp
 """
 
 import requests
 from lxml import html
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
+import re
 
-"""
-response = requests.get("https://en.wikipedia.org/wiki/List_of_countries_by_population_in_2010")
-
-tree = html.fromstring(response.text)
-countries = tree.xpath('//span[@class="flagicon"]')
-for country in countries:
-    print(country.xpath('./following-sibling::a/text()')[0])
-
-url = "https://www.wsj.com/market-data/quotes/company-list"
-url = "https://www.nasdaq.com/market-activity/cryptocurrency/btc/historical"
-
-# Boiler plate below
-response = requests.get(url)
-tree = html.fromstring(response.text)
-
-# Searching for the sector:
-# sectors = tree.xpath('//div[@class="index-sector border-box"]')
-# print(type(sectors))
-
-price_data = tree.xpath('//tbody[@class="historical-data__table-body"]')
-print(price_data)
-
-#for sector in sectors:
-#    print(sector)
-"""
-
+# Sneaky little deception. Credit to Bowen
 headers = {
-     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) '
+                   'Chrome/75.0.3770.142 Safari/537.36'
 }
 
 subj_list = requests.get(f"https://www.wsj.com/market-data/quotes/company-list", headers=headers).content
-soup = BeautifulSoup(subj_list, features="html.parser")
+soup = bs(subj_list, features="html.parser")
 
-print(soup.find("ul", {"class": "cl-tree cl-list"}))
+# Searching for the
+items = soup.find("ul", {"class": "cl-tree cl-list"})
+
+links = items.find_all("a", attrs={'href': re.compile("^https://")})
+sector_url = ""
+for link in links:
+    sector_url = (link.get('href'))
+
+# gets the name of all stock
+sector_list = requests.get(sector_url, headers=headers).content
+sector_soup = bs(sector_list, features="html.parser")
+stock_list = sector_soup.find("table", class_="cl-table")
+stock_links = stock_list.find_all("a", attrs={'href': re.compile("^https://")})
+for stock_url in stock_links:
+    print(stock_url.get('href'))
